4d8b6086a108bdf8ee761824456954f1c58f7e6b
django-json <diango.jayson@gmail.com>
2020-03-03 16:00:35 +0800

    Updates

---
+++ precache-manifest.fa734a012e7ca7a13b79c7fd23352908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4800f110780fd15e5ced299ab3cd373",
+    "url": "/simple-react-calculator/index.html"
+  },
+  {
+    "revision": "e518b51bc3e6b013fe16",
+    "url": "/simple-react-calculator/static/css/main.6d1a9a3b.chunk.css"
+  },
+  {
+    "revision": "2acf8e0f86adf904c719",
+    "url": "/simple-react-calculator/static/js/2.ae8c526e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/simple-react-calculator/static/js/2.ae8c526e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e518b51bc3e6b013fe16",
+    "url": "/simple-react-calculator/static/js/main.b12fbc6d.chunk.js"
+  },
+  {
+    "revision": "d5d7a6db6de1686b8cd8",
+    "url": "/simple-react-calculator/static/js/runtime-main.c0d3b321.js"
+  }
+]);
